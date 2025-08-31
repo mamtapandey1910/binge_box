21cf31e24db905d8533ef67dbc7844d0c15ecac2
Mamta Pandey <mamtapandey@MacBookAir.Home>
2025-08-31 11:57:21 +0100

    initial commit and navbar

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { Navbar } from '@/components/navbar/navbar'
+
+export default function Home() {
+  return (
+    <h2>Welcome to home page</h2>
+  );
+}
---
+++ components/register/register.js
@@ -0,0 +1,6 @@
+export const RegisterComponent = () => {
+    return (
+        <div style={{ marginTop: '4rem' }}>Hello, I am register page </div>
+
+    )
+}
